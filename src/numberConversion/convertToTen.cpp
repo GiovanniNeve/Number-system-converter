170c78497735210ac43a4a7182bc228cba595518
GiovanniNeve <thomasz01@outlook.it>
2020-10-12 12:01:43 +0200

    Add: Convert fractorial number with over ten base to ten base

--- src/numberConversion/convertToTen.cpp
+++ src/numberConversion/convertToTen.cpp
@@ -1,37 +1,75 @@
 #include "../include/includeLibrary.h"
 
-//** Convert a non decimal number in a decimal number
-static std::string convertToTen(std::string stringNumber, int initialBase) {
-    std::vector<int> decimalNumber; 
-    std::string numTemp;
-    int sum = 0;
-    //** Initialize length value at -1
-    int length = -1;
+inline double convertToTenNumber(std::string number, int initialBase, int length) {
+    std::vector<int> decimalNumber;
+    double sum = 0;
 
     //** Loop for each char in the number
-    for(char c : stringNumber) {
+    for (char c : number) {
+
         //** Check if the number is an alphabet letter
-        if(std::isalpha(c))  {
+        if (std::isalpha(c)) {
             //** If so, find it in the lettes array returning his iterator
             char* i = std::find(letters, end, c);
             //** Then calculate his intger value and insert in a vector
-            decimalNumber.push_back(i - letters + 10);
-        } else {
+            decimalNumber.push_back(i-letters+10);
+        }
+        else {
             //** If the number is an actual number convert from char to string
             std::string temp(1, c);
             //** Convert string to int and insert in a vector
             decimalNumber.push_back(std::stoi(temp));
         }
-        length++;
+
     }
 
     //** Loop for each number in the vector
     for(auto c : decimalNumber) {
+        length -= 1;
         //** Sum every number multiplied by inital base pow the position of the number
         sum += (c * std::pow(initialBase, length));
-        length--;
     }
-    
+
     //** Convert sum to string
-    return std::to_string(sum);
+    return sum;
 }
+
+//** Convert a non decimal number in a decimal number
+static std::string convertToTen(std::string stringNumber, int initialBase) {
+    int intgerSum;
+    double fractorialSum = 0.0;
+    int stringLength = stringNumber.length();
+    std::string result;
+    std::string numTemp;
+    std::string intgerNumber;
+
+    if(stringNumber.find('.', 0) != std::string::npos) {
+        std::string fractorialNumber;
+        std::string stringTemp = stringNumber;
+
+        size_t dotPosition = stringTemp.find_first_of('.');
+        //** Leave only the intger part in the string
+        stringTemp.erase(dotPosition);
+        //** Store the intger part in a variable
+        intgerNumber = stringTemp;
+        //** Reset string template
+        stringTemp = stringNumber;
+        //** Leave only the fractorial part in the string
+        stringTemp.erase(stringTemp.begin(), stringTemp.end() - (stringLength - dotPosition - 1));
+        //** Store the fractorial part in a variable
+        fractorialNumber = stringTemp;
+        fractorialSum = convertToTenNumber(fractorialNumber, initialBase, 0);
+
+    } else {
+        intgerNumber = stringNumber;
+
+    }
+
+    intgerSum = convertToTenNumber(intgerNumber, initialBase, intgerNumber.length());
+
+    result = (fractorialSum != 0.0) ? std::to_string(intgerSum + fractorialSum) : std::to_string(intgerSum);
+
+    return result;
+}
+
+
