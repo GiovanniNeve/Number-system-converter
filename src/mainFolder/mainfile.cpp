bccd2fc1487f01e0a867dbc3d50ba42f16858eed
GiovanniNeve <thomasz01@outlook.it>
2020-10-15 15:44:33 +0200

    Code refractor

--- src/mainFolder/mainfile.cpp
+++ src/mainFolder/mainfile.cpp
@@ -7,8 +7,7 @@
 */
 
 #include "../numberCheck/numberCheck.cpp"
-#include "../numberConversion/convertToOverTenBase.cpp"
-#include "../numberConversion/convertToUnderTenBase.cpp"
+#include "../numberConversion/convertToNewBase.cpp"
 #include "../numberConversion/convertToTen.cpp"
 
 //** Initialization of global variables
@@ -21,7 +20,8 @@ int main() {
     
     //** Try condition
     try {
-        std::cout << "\nWARNING: At the moment the number this converter can take fractorial numbers only\nfor 10 to <10 base conversion, fractorial numbers will be implementerd soon.\n\n";
+        std::cout
+                << "\nWARNING: At the moment the number this converter can take fractorial numbers only\nfor 10 to <10 base conversion, fractorial numbers will be implementerd soon.\n\n";
 
         std::string number;
         std::string result;
@@ -40,26 +40,21 @@ int main() {
         numberCheck(number, initialBase, newBase);
 
         //** Check if number has to be caonverted from base 10 to base under 10
-        if(initialBase == 10 && newBase < 10) {
+        if (initialBase==10 && newBase!=10) {
             //** Call convertToUnderTenBase()
-            std::cout << convertToUnderTenBase(number, newBase);
+            std::cout << convertToNewBase(number, newBase);
 
-        //** Check if number has to be caonverted from base 10 to base over 10
-        } else if(initialBase == 10 && newBase > 10) {
-            //** Call convertToOverTenBase()
-            std::cout << convertToOverTenBase(number, newBase);
-
-                //** Check if number has to be caonverted from base != 10 to base 10
+        //** Check if number has to be caonverted from base != 10 to base 10
         } else if (initialBase != 10 && newBase == 10) {
             //** Call convertToTen()
             std::cout << convertToTen(number, initialBase);
 
-                //** Check if number has to be caonverted from base != 10 to base != 10
+        //** Check if number has to be caonverted from base != 10 to base != 10
         } else if(initialBase != 10 && newBase != 10) {
-            //** Call convertToTen()
+            //** Convert number to ten base
             result = convertToTen(number, initialBase);
-            //** If new base is under 10 call convertToUnderTenBase(), if it is over 10 call convertToOverTenBase()
-            (newBase < 10) ? std::cout << convertToUnderTenBase(result, newBase) : std::cout << convertToOverTenBase(result, newBase);
+            //** Concert number to the new base
+            result = convertToNewBase(result, newBase);
         }
     } 
 
